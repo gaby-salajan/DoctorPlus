2346a3b88a713d2a8ec75d574e95480d4ddb0d6b
Gaby Salajan <gaby.salajan14@gmail.com>
2022-11-08 16:06:26 +0200

    -modificare pagina intro

--- app/src/main/java/com/ns/doctorplus/IntroActivity.java
+++ app/src/main/java/com/ns/doctorplus/IntroActivity.java
@@ -2,7 +2,12 @@ package com.ns.doctorplus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class IntroActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        ImageView splashImg = findViewById(R.id.logoView);
+
+        Animation imgAnimation = AnimationUtils.loadAnimation(this, R.anim.intro_img_animation);
+
+        splashImg.setAnimation(imgAnimation);
+
+        new CountDownTimer(5000, 1000) {
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                startActivity(new Intent(IntroActivity.this, LoginActivity.class));
+                finish();
+            }
+        }
+                .start();
     }
 }
