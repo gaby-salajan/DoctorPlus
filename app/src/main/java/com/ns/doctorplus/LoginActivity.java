5a513ba9eb3b0bde40824cdfb0660940a1853b97
Gaby Salajan <gaby.salajan14@gmail.com>
2022-11-08 19:05:58 +0200

    -modificare pagina intro

--- app/src/main/java/com/ns/doctorplus/LoginActivity.java
+++ app/src/main/java/com/ns/doctorplus/LoginActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+
 public class LoginActivity extends AppCompatActivity {
 
     private Button btnRegister;
