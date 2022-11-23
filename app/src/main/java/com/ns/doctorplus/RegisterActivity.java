c50a38dc55bf9ba7a543a3d6ba59bbee8b9ab870
Gabriel Salajan <gaby.salajan14@gmail.com>
2022-11-23 17:17:57 +0200

    -adaugat Date Picker

--- app/src/main/java/com/ns/doctorplus/RegisterActivity.java
+++ app/src/main/java/com/ns/doctorplus/RegisterActivity.java
@@ -3,18 +3,16 @@ package com.ns.doctorplus;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.app.DatePickerDialog;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.TextView;
 
-import com.afollestad.materialdialogs.*;
+import com.google.android.material.datepicker.MaterialDatePicker;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.concurrent.atomic.AtomicInteger;
+
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -31,28 +29,24 @@ public class RegisterActivity extends AppCompatActivity {
         registerButton = findViewById(R.id.registerButton);
         fieldBirthDate = findViewById(R.id.fieldBirthDate);
 
-        fieldBirthDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Calendar cal = new GregorianCalendar();
-                AtomicInteger year = new AtomicInteger(cal.get(Calendar.YEAR));
-                AtomicInteger month = new AtomicInteger(cal.get(Calendar.MONTH));
-                AtomicInteger day = new AtomicInteger(cal.get(Calendar.DAY_OF_MONTH));
-
-                DatePickerDialog datePicker = new DatePickerDialog(RegisterActivity.this,
-                        R.style.Theme_DoctorPlus,
-                        (datePicker1, y, m, d) -> {
-                            year.set(y);
-                            month.set(m);
-                            day.set(d);
-                        },
-                        year.get(),
-                        month.get(),
-                        day.get());
-
-                datePicker.updateDate(1988,9,8);
-            }
+        fieldBirthDate.setOnClickListener(view -> {
+            showDatePickDialog();
         });
+    }
+
+    public void showDatePickDialog(){
+        MaterialDatePicker<?> picker = MaterialDatePicker.Builder
+                .datePicker()
+                .setTitleText("Select date of birth")
+                .build();
+
+        picker.addOnPositiveButtonClickListener(selection -> {
+            @SuppressLint("SimpleDateFormat")
+            SimpleDateFormat simpleFormat = new SimpleDateFormat("dd/MM/yyyy");
+            fieldBirthDate.setText(simpleFormat.format(selection));
+        });
+        picker.show(getSupportFragmentManager(), "DATE_PICKER");
+
 
     }
 }
